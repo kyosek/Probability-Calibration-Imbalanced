0db20b8e2251bf8cb7ad44211cdbb8fa5880633f
Kyosuke Morita <kyosuke1029@icloud.com>
2020-01-15 19:05:29 +0000

    Update logistics.py

--- src/functionalscripts/logit_example/logistics.py
+++ src/functionalscripts/logit_example/logistics.py
@@ -5,7 +5,7 @@ logit = LogisticRegression(random_state=42, solver='lbfgs',max_iter=1000)
 tr_x_rus, tr_y_rus = rus.fit_resample(tr.drop(['Class'],1),tr.Class)
 logit_rus = logit.fit(tr_x_rus,tr_y_rus)
 
-# RUSBoost
+# RUSBagging
 bc = BalancedBaggingClassifier(base_estimator=logit,random_state=42)
 logit_bc = bc.fit(tr.drop(['Class'],1),tr.Class)
 
@@ -15,6 +15,6 @@ tau = BMR.tau(tr.Class)
 # with rundom undersampling
 y_pred_calib_rus = BMR.calibration(prob=logit_rus.predict_proba(te.drop(['Class'],1))[:,1],beta=beta)
 y_predicted_calib_rus = np.where(y_pred_calib_rus>=.99,1,0)
-# with RUSBoost
+# with RUSBagging
 y_pred_calib_bc = BMR.calibration(prob=logit_bc.predict_proba(te.drop(['Class'],1))[:,1],beta=beta)
 y_predicted_calib_bc = np.where(y_pred_calib_bc>=tau,1,0)
